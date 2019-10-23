9f2d670afe3f45ba00950fe24be313a8f43c4aee
Jake Zhou <xz346@scarletmail.rutgers.edu>
2019-10-23 19:04:31 -0400

    Created basic framework for android app

---
+++ app/src/main/java/com/study/activities/Bindr.java
@@ -0,0 +1,14 @@
+package com.study.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bindr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
