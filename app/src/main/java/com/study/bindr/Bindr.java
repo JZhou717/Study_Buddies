1494ebfb9d48b57c1f284218979fa0f32b076edd
Jake Zhou <xz346@scarletmail.rutgers.edu>
2019-10-26 19:19:41 -0400

    Login Screen now changes screen. Navigation icons now change screens

--- app/src/main/java/com/study/bindr/Bindr.java
+++ app/src/main/java/com/study/bindr/Bindr.java
@@ -1,14 +1,20 @@
 package com.study.bindr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class Bindr extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Bindr extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     //Need this for our drawer layout
     private DrawerLayout drawer;
 
@@ -17,25 +23,62 @@ public class Bindr extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Need this line to make toolbar navbar working
+
+        /* Start Navigation Stuff */
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         drawer = findViewById(R.id.login_screen);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        /* End Navigation Stuff */
 
     }
 
-    //So Navbar closes on activity change
+    public void login(View view) {
+        Intent intent = new Intent(Bindr.this, EditCoursesActivity.class);
+        startActivity(intent);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(Bindr.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    /* Start Navigation Stuff */
+    //Navbar closes on activity change
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-
             super.onBackPressed();
         }
     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Intent intent;
+
+        switch (item.getItemId()) {
+            case R.id.nav_courses:
+                intent = new Intent(Bindr.this, EditCoursesActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_profile:
+                intent = new Intent(Bindr.this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+
+        return true;
+    }
+
+    /* End Navigation Stuff */
 }
