1e513d2f97c03214a9c0c371ee5926a28cf780ef
Jake Zhou <xz346@scarletmail.rutgers.edu>
2019-10-26 17:24:11 -0400

    Added in nav bar features onto login screen

--- app/src/main/java/com/study/bindr/Bindr.java
+++ app/src/main/java/com/study/bindr/Bindr.java
@@ -1,14 +1,41 @@
 package com.study.bindr;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class Bindr extends AppCompatActivity {
+    //Need this for our drawer layout
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Need this line to make toolbar navbar working
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.login_screen);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    //So Navbar closes on activity change
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+
+            super.onBackPressed();
+        }
     }
 }
