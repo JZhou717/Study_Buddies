3d7fa42c0dde68b4526e1303fd7a91940b947ade
Feiying Zheng <feiying.zheng.ths16@gmail.com>
2019-10-26 21:18:16 -0400

    added chats list and chatbox skeletons

---
+++ app/src/main/java/com/study/bindr/ChatsListActivity.java
@@ -0,0 +1,83 @@
+package com.study.bindr;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class ChatsListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //Need this for our drawer layout
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats_list);
+
+        /* Start Navigation Stuff */
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Change the view to the proper screen
+        drawer = findViewById(R.id.chatslist_screen);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //Change this to the proper page
+        navigationView.setCheckedItem(R.id.nav_home);
+        /* End Navigation Stuff */
+    }
+
+    /* Start Navigation Stuff */
+    //Navbar closes on activity change
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Intent intent;
+
+        switch (item.getItemId()) {
+            case R.id.nav_courses:
+                intent = new Intent(ChatsListActivity.this, EditCoursesActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_profile:
+                intent = new Intent(ChatsListActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_home:
+                intent = new Intent(ChatsListActivity.this, Home_Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_session:
+                intent = new Intent(ChatsListActivity.this, Session_Activity.class);
+                startActivity(intent);
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /* End Navigation Stuff */
+}
