39604c649a66cffe467ef75e97b36493e3026d4a
kac460 <kac460@rutgers.edu>
2019-10-28 00:14:27 -0400

    User Profile

--- app/src/main/java/com/study/bindr/UserProfileActivity.java
+++ app/src/main/java/com/study/bindr/UserProfileActivity.java
@@ -1,6 +1,19 @@
 package com.study.bindr;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.text.method.ScrollingMovementMethod;
 
 public class UserProfileActivity extends Activity{
+    private TextView bioTextView;
+    private TextView interestsTextView;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_user_profile);
+        bioTextView = (TextView)findViewById(R.id.textViewBio);
+        interestsTextView = (TextView)findViewById(R.id.textViewInterests);
+        bioTextView.setMovementMethod(new ScrollingMovementMethod());
+        interestsTextView.setMovementMethod(new ScrollingMovementMethod());
+    }
 }
