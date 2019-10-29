9a27aaef2c72c8202f0d0bf1dc594898e057aa7f
Feiying Zheng <feiying.zheng.ths16@gmail.com>
2019-10-29 16:42:16 -0400

    fixed user profile spacing, adding back option in rate study session tool bar

--- app/src/main/java/com/study/bindr/RateActivity.java
+++ app/src/main/java/com/study/bindr/RateActivity.java
@@ -13,6 +13,7 @@ public class RateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Rate this Study Session");
 
     }
