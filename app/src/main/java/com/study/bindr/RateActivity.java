885db5bedff2bfe0f5e7099eff6e6353178aaf4c
Jake Zhou <xz346@scarletmail.rutgers.edu>
2019-10-29 15:26:44 -0400

    Edited Rate Activity and its layout

--- app/src/main/java/com/study/bindr/RateActivity.java
+++ app/src/main/java/com/study/bindr/RateActivity.java
@@ -10,5 +10,7 @@ public class RateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
+        getSupportActionBar().setTitle("Rate this Study Session");
+
     }
 }
