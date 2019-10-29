9dd42a93be54ea68afc4b0b407006b05a1d6a7f0
kac460 <kac460@rutgers.edu>
2019-10-28 23:24:57 -0400

    Touched up Edit Courses and made next button of Register functional

--- app/src/main/java/com/study/bindr/RegisterActivity.java
+++ app/src/main/java/com/study/bindr/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.study.bindr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
 
+    public void goToEditCourses(View view) {
+        Intent intent = new Intent(RegisterActivity.this, EditCoursesActivity.class);
+        startActivity(intent);
     }
 }
