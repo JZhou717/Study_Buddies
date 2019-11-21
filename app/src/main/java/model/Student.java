85c5d1abda771acdcd8980f57bd99db38fc39c57
kac460 <kac460@rutgers.edu>
2019-11-20 23:45:29 -0500

    Skeleton for Student

--- app/src/main/java/model/Student.java
+++ app/src/main/java/model/Student.java
@@ -4,17 +4,109 @@ import java.util.List;
 
 public class Student {
 
-    String firstName;
-    String lastName;
-    List<String> courses;
+    private List<Course> courses;
+    private List<String> chatRooms;
+    private List<Session> sessions;
+    private List<Integer> matched;
+    private List<Integer> passed;
+    private List<Integer> pendingMatches;
+    private int id;
 
-    public Student(String firstName, String lastName){
-        this.firstName=firstName;
-        this.lastName=lastName;
+
+    public Student(int id){
+        this.id = id;
+        //TODO: Retrieve student's courses, chatRooms, sessions, matched, passed from DB
     }
 
-    public String getFullName(){
-        return firstName+" "+lastName;
+    public String getEmail(){
+        return null;
     }
 
+    public List<String> getChatRooms(){
+        return chatRooms;
+    }
+
+    public List<Session> getSessions(){
+        return sessions;
+    }
+
+    public List<Course> getCourses(){
+        return courses;
+    }
+
+    public List<Integer> getMatched(){
+        return matched;
+    }
+
+    public List<Integer> getPassed(){
+        return passed;
+    }
+
+    public List<Integer> getPendingMatches(){
+        return pendingMatches;
+    }
+
+    public String getUsername(){
+        return null;  //TODO: GET FROM DB
+    }
+
+    public String getPassword(){
+        return null;  //TODO: GET FROM DB
+    }
+
+    public String getName(){
+        return null;  //TODO: GET FROM DB
+    }
+
+    public String getBio(){
+        return null; //TODO: GET FROM DB
+    }
+
+    public double getGPA(){
+        return -1;  //TODO: GET FROM DB
+    }
+
+    public void addChatRoom(String room){
+        //TODO: IMPLEMENT
+    }
+
+    public void editEmail(String newEmail){
+        //TODO: IMPLEMENT
+    }
+
+    public void editPassword(String newPassword){
+        //TODO: IMPLEMENT
+    }
+
+    public void editName(String newName){
+        //TODO: IMPLEMENT
+    }
+
+    public void editBio(String newBio){
+        //TODO: IMPLEMENT
+    }
+
+    public void editGPA(double newGPA){
+        //TODO: IMPLEMENT
+    }
+
+    public void setStatus(boolean isActive){
+        //TODO: IMPLEMENT
+    }
+
+    public void deleteAccount(){
+        //TODO: IMPLEMENT
+    }
+
+    public void populateStudentInfo(){
+        //TODO: IMPLEMENT
+    }
+
+    public void addPassedStudent(int passedStudentID){
+        //TODO: IMPLEMENT
+    }
+
+    public void addMatchedStudent(int matchedStudentID){
+        //TODO: IMPLEMENT
+    }
 }
