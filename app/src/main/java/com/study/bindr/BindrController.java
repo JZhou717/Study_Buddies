4f318c99a793253352cfcbf141267ab6d8da4560
kac460 <kac460@rutgers.edu>
2019-11-25 15:15:22 -0500

    BindrController

--- app/src/main/java/com/study/bindr/BindrController.java
+++ app/src/main/java/com/study/bindr/BindrController.java
@@ -1,5 +1,77 @@
 package com.study.bindr;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.android.core.auth.StitchUser;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteFindIterable;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
+import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
+
+import org.bson.Document;
+
 public class BindrController {
-    //hold db, collections
+    final StitchAppClient client =
+            Stitch.initializeDefaultAppClient("bindr-yfpaz");
+
+    final RemoteMongoClient mongoClient =
+            client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+
+    final RemoteMongoCollection<Document> coll =
+            mongoClient.getDatabase("<DATABASE>").getCollection("<COLLECTION>");
+
+client.getAuth().loginWithCredential(new AnonymousCredential()).continueWithTask(
+        new Continuation<StitchUser, Task<RemoteUpdateResult>>() {
+
+        @Override
+        public Task<RemoteUpdateResult> then(@NonNull Task<StitchUser> task) throws Exception {
+            if (!task.isSuccessful()) {
+                Log.e("STITCH", "Login failed!");
+                throw task.getException();
+            }
+
+            final Document updateDoc = new Document(
+                    "owner_id",
+                    task.getResult().getId()
+            );
+
+            updateDoc.put("number", 42);
+            return coll.updateOne(
+                    null, updateDoc, new RemoteUpdateOptions().upsert(true)
+            );
+        }
+    }
+).continueWithTask(new Continuation<RemoteUpdateResult, Task<List<Document>>>() {
+        @Override
+        public Task<List<Document>> then(@NonNull Task<RemoteUpdateResult> task) throws Exception {
+            if (!task.isSuccessful()) {
+                Log.e("STITCH", "Update failed!");
+                throw task.getException();
+            }
+            List<Document> docs = new ArrayList<>();
+            return coll
+                    .find(new Document("owner_id", client.getAuth().getUser().getId()))
+                    .limit(100)
+                    .into(docs);
+        }
+    }).addOnCompleteListener(new OnCompleteListener<List<Document>>() {
+        @Override
+        public void onComplete(@NonNull Task<List<Document>> task) {
+            if (task.isSuccessful()) {
+                Log.d("STITCH", "Found docs: " + task.getResult().toString());
+                return;
+            }
+            Log.e("STITCH", "Error: " + task.getException().toString());
+            task.getException().printStackTrace();
+        }
+    });
 }
