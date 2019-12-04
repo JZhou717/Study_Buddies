dea095a601650f6035c61baf99641507d4f403ce
kac460 <kac460@rutgers.edu>
2019-12-04 14:27:46 -0500

    Implemented addStudentToThisCourseInDatabase()

--- app/src/main/java/model/Course.java
+++ app/src/main/java/model/Course.java
@@ -8,6 +8,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.mongodb.client.model.Filters;
 import com.mongodb.stitch.core.services.mongodb.remote.RemoteFindOptions;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteInsertOneResult;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
 import com.study.bindr.BindrController;
 import com.study.bindr.DatabaseCallBack;
 
@@ -15,6 +17,7 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Course {
@@ -90,6 +93,105 @@ public class Course {
     }
 
     public void addStudentToThisCourseInDatabase(String studentID){
-        //TODO: Implement
+        Bson filter = Filters.and(
+                Filters.eq("schoolID", schoolID),
+                Filters.eq("departmentID", departmentID),
+                Filters.eq("courseID", departmentID));
+
+        //Project just the id
+        Document projection = new Document()
+                .append("_id", 0);
+
+        RemoteFindOptions options = new RemoteFindOptions()
+                .projection(projection);
+
+        //task to find ids of students in this course
+        final Task <Document> findCourse = BindrController.studentsCollection
+                .findOne(filter, options);
+
+        //listens for when the query finishes and updates flag courseExistsInCollection
+        findCourse.addOnCompleteListener(new OnCompleteListener <Document> () {
+            @Override
+            public void onComplete(@NonNull Task <Document> task) {
+                final Document newCourse = new Document()
+                        .append("schoolID", schoolID)
+                        .append("departmentID", departmentID)
+                        .append("courseID", courseID)
+                        .append("students", Arrays.asList(new ObjectId(studentID)));
+                if (task.getResult() == null) {
+                    Log.d("findCourse",
+                            String.format("No document matches the provided query"));
+                    final Task <RemoteInsertOneResult> insertNewCourse
+                            = BindrController.coursesCollection.insertOne(newCourse);
+                    insertNewCourse.addOnCompleteListener(new OnCompleteListener <RemoteInsertOneResult> ()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task <RemoteInsertOneResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("insertNewCourse",
+                                        String.format("successfully inserted item with id %s",
+                                        task.getResult().getInsertedId()));
+                                Log.d("insertNewCourse", String.format("Course SAVED"));
+
+                            } else {
+                                Log.e("insertNewCourse",
+                                        "failed to insert document with: ",
+                                        task.getException());
+                            }
+                        }
+                    });
+                }
+                else if (task.isSuccessful()) {
+                    Log.d("findCourse",
+                            String.format("Successfully found document: %s", task.getResult()));
+                    Document courseDocumentIDAsDocument = task.getResult();
+                    Document studentsArrayWithStudentToAddDocument = new Document()
+                            .append("students", studentID);
+                    Document updateCourseStudentsArrayDocument = new Document()
+                            .append("$addToSet", studentsArrayWithStudentToAddDocument);
+
+                    //add the course to the array
+                    Task<RemoteUpdateResult> updateCourseStudentsArray =
+                            BindrController.coursesCollection.updateOne(
+                                    courseDocumentIDAsDocument, updateCourseStudentsArrayDocument);
+                    updateCourseStudentsArray.addOnCompleteListener(new OnCompleteListener<RemoteUpdateResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<RemoteUpdateResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("updateCourse","successfully updated course");
+
+                            } else {
+                                Log.e("updateCourse","failed to update course with: ",
+                                        task.getException());
+                            }
+                        }
+                    });
+
+                } else {
+                    Log.e("findCourse",
+                            "Failed to findOne: ", task.getException());
+                    final Task <RemoteInsertOneResult> insertNewCourse
+                            = BindrController.coursesCollection.insertOne(newCourse);
+                    insertNewCourse.addOnCompleteListener(new OnCompleteListener <RemoteInsertOneResult> ()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task <RemoteInsertOneResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("insertNewCourse",
+                                        String.format("successfully inserted item with id %s",
+                                                task.getResult().getInsertedId()));
+                                Log.d("insertNewCourse", String.format("Course SAVED"));
+
+                            } else {
+                                Log.e("insertNewCourse",
+                                        "failed to insert document with: ",
+                                        task.getException());
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        while(!findCourse.isComplete());
     }
 }
