26fd6d840fb7f2739bfd27fa4f14df5eb3513cce
Jake Zhou <xz346@scarletmail.rutgers.edu>
2019-12-07 23:39:20 -0500

    Implemented part of register

--- app/src/main/java/com/study/bindr/DatabaseUtility.java
+++ app/src/main/java/com/study/bindr/DatabaseUtility.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteFindIterable;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteFindOptions;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -59,6 +60,82 @@ public class DatabaseUtility {
 
                 } else {
                     Log.e("getFullNameList", "failed to find documents with: ", task.getException());
+                }
+            }
+        });
+    }
+
+    /**
+     * Runs a query to see if the inputted email is already taken by another user
+     * @param email the email that we are checking for in the database
+     * @param dbCallBack the method that we pass a boolean to on query completion
+     */
+    protected static void emailTaken(String email, DatabaseCallBack<Boolean> dbCallBack) {
+        //Query by field
+        Document query = new Document().append("email", email);
+        //Hide the id
+        Document projection = new Document()
+                .append("_id", 0);
+        RemoteFindOptions options = new RemoteFindOptions()
+                .projection(projection);
+
+        final Task<Document> findTask = BindrController.studentsCollection.findOne(query, options);
+
+        //listens for when the query finishes and sends result to callback method (given in parameter)
+        findTask.addOnCompleteListener(new OnCompleteListener<Document>() {
+            @Override
+            public void onComplete(@NonNull Task <Document> task) {
+                if (task.getResult() == null) {
+                    Log.d("emailTaken", "Email has not been taken");
+                    //Email not taken
+                    dbCallBack.onCallback(new Boolean(false));
+                }
+                else if (task.isSuccessful()) {
+                    Log.d("emailTaken", String.format("Successfully found document: %s",
+                            task.getResult()));
+                    //Email taken
+                    dbCallBack.onCallback(new Boolean(true));
+
+                } else {
+                    Log.e("emailTaken", "Failed to findOne: ", task.getException());
+                }
+            }
+        });
+    }
+
+    /**
+     * Runs a query to see if the inputted username is already taken by another user
+     * @param username the username that we are checking for in the database
+     * @param dbCallBack the method that we pass a boolean to on query completion
+     */
+    protected static void usernameTaken(String username, DatabaseCallBack<Boolean> dbCallBack) {
+        //Query by field
+        Document query = new Document().append("username", username);
+        //Hide the id
+        Document projection = new Document()
+                .append("_id", 0);
+        RemoteFindOptions options = new RemoteFindOptions()
+                .projection(projection);
+
+        final Task<Document> findTask = BindrController.studentsCollection.findOne(query, options);
+
+        //listens for when the query finishes and sends result to callback method (given in parameter)
+        findTask.addOnCompleteListener(new OnCompleteListener<Document>() {
+            @Override
+            public void onComplete(@NonNull Task <Document> task) {
+                if (task.getResult() == null) {
+                    Log.d("usernameTaken", "Username has not been taken");
+                    //Username not taken
+                    dbCallBack.onCallback(new Boolean(false));
+                }
+                else if (task.isSuccessful()) {
+                    Log.d("usernameTaken", String.format("Successfully found document: %s",
+                            task.getResult()));
+                    //Username taken
+                    dbCallBack.onCallback(new Boolean(true));
+
+                } else {
+                    Log.e("usernameTaken", "Failed to findOne: ", task.getException());
                 }
             }
         });
--- app/src/main/java/com/study/bindr/RegisterActivity.java
+++ app/src/main/java/com/study/bindr/RegisterActivity.java
@@ -1,11 +1,14 @@
 package com.study.bindr;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import model.Student;
@@ -58,10 +61,63 @@ public class RegisterActivity extends AppCompatActivity {
         String fullName = fullNameField.getText().toString();
         String bio = bioField.getText().toString();
         String interests = interestsField.getText().toString();
-        String gpa = GPAField.getText().toString();
+        String gpaString = GPAField.getText().toString();
+        Double gpa;
 
-        
+        /* * * Validate fields * * */
+        //Check email
+        if(email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+            createAlert("Invalid email input", "Please enter a valid email");
+            return;
+        }
+        //Check if email is taken
+        DatabaseUtility.emailTaken(email, new DatabaseCallBack<Boolean>() {
+            @Override
+            public void onCallback(Boolean taken) {
+                if(taken) {
+                    createAlert("Email Taken", "Please enter a new unique email");
+                    return;
+                }
+            }
+        });
 
+        //Check username
+        if(username.equals("")) {
+            createAlert("Invalid username input", "Please enter a valid username");
+            return;
+        }
+        //Check if username is taken
+        DatabaseUtility.usernameTaken(username, new DatabaseCallBack<Boolean>() {
+            @Override
+            public void onCallback(Boolean taken) {
+                if(taken) {
+                    createAlert("Username Taken", "Please enter a new unique username");
+                    return;
+                }
+            }
+        });
+
+        //Check password
+        if(password.equals("")) {
+            createAlert("Invalid password input", "Please enter a valid password");
+            return;
+        }
+        if(!password.equals(confirmPassword)) {
+            createAlert("Invalid password input", "Please ensure your password matches your confirmation password");
+            return;
+        }
+
+        //Check gpa
+        if(gpaString.equals("")) {
+            gpaString = "0";
+        }
+        try {
+            gpa = Double.parseDouble(gpaString);
+        } catch (NumberFormatException e) {
+            createAlert("Invalid gpa input", "Please input a valid gpa of the format x.y where x and y are whole numbers");
+            return;
+        }
+        /* * * End of validating fields * * */
     }
 
     /**
@@ -69,6 +125,33 @@ public class RegisterActivity extends AppCompatActivity {
      */
     public void uploadImage(View view) {
         //TODO: IMPLEMENT THIS
+
+
+
+
+
+
+
+
+    }
+
+    /**
+     * Helper method that creates an alert dialog with the given title and message
+     * Alert has the OK button
+     * @param title Title of the alert
+     * @param message Message in the alert
+     */
+    private void createAlert(String title, String message) {
+        AlertDialog alert = new AlertDialog.Builder(RegisterActivity.this).create();
+        alert.setTitle(title);
+        alert.setMessage(message);
+        alert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alert.show();
     }
 
     //Transitions to the edit course activity, called when the account creation is successful
