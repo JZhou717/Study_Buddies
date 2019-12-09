260b491c52eb0c3662ebd1585347917589f345b0
Jack <jack@nbp-30-178.nbp.ruw.rutgers.edu>
2019-12-08 23:00:52 -0500

    tests

--- app/src/test/java/model/SessionTest.java
+++ app/src/test/java/model/SessionTest.java
@@ -3,29 +3,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 
 public class SessionTest {
 
     private Session session;
-    private final Student[] students = new Student[2];
-    private final LocalDateTime dateTime = LocalDateTime.now();
+    private final String sampleID = "test";
+    private final Date dateTime = new Date();
+    private final int reminder = 0;
 
 
     @Before
     public void setup(){
-        session = new Session(students,dateTime);
+        session = new Session(sampleID,dateTime,reminder);
     }
 
     @Test
-    public void testGetStudents(){
-        assertTrue(students.equals(session.getStudents()));
+    public void testGetPartnerID(){
+        assertTrue(sampleID.equals(session.getPartnerID()));
     }
 
     @Test
     public void testGetDateTime(){
         assertTrue(dateTime.equals(session.getDateTime()));
+    }
+
+    @Test
+    public void testGetReminder(){
+        assertTrue(reminder == session.getReminder());
     }
 }
 
